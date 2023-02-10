8b81caf75f66c51703e5be0f857bb9c0d058687b
Luu The Vinh <luuthevinh1988@gmail.com>
2023-02-10 22:44:48 +0700

    initial project

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+      <header className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'></div>
+            <div className='col-6'></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
